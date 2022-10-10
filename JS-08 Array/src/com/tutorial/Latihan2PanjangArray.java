65d93c7d375c1b624c4ee5bd11761c57ef41c085
21343001-adelinasuciko <112172809+21343001-adelinasuciko@users.noreply.github.com>
2022-10-10 17:17:05 +0700

    Add files via upload

---
+++ JS-08 Array/src/com/tutorial/Latihan2PanjangArray.java
@@ -0,0 +1,19 @@
+/*
+ * Created by: 21343001_ADELINA SUCIKO 
+ * Percobaan 2 panjang array
+ */
+
+package com.tutorial;
+
+
+public class Latihan2PanjangArray {
+
+	public static void main(String[] args) {
+		int[] ages = new int[100];
+		for(int i=0; i<ages.length; i++ ) {
+			System.out.print(ages[i]);
+		}
+
+	}
+
+}
---
+++ JS-08 Array/src/com/tutorial/Tugas1.java
@@ -0,0 +1,20 @@
+/*
+ * Creted by: 21343001_Adelina Suciko
+ * program menampilkan hari dalam minggu
+ */
+
+package com.tutorial;
+
+public class Tugas1 {
+
+	public static void main(String[] args) {
+		
+		String[] days = {"senin","selasa","Rabu","kamis", "jumat", "sabtu"};
+		for(int i=0; i<days.length; i++){
+			System.out.println(""+days[i]);
+		}
+
+
+	}
+
+}
